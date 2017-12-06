a1198d14712a0d64e5d657bdae6a6b1dc30416f0
Gris Ge <fge@redhat.com>
2017-12-06 21:57:23 +0800

    lvm_tp.sh: Improve script for better clean up.
    Signed-off-by: Gris Ge <fge@redhat.com>

--- lvm_tp.sh
+++ lvm_tp.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 
-if [ "CHK$(lvs|grep ThinPoolLV)" == "CHK" ];then
-    sudo modprobe scsi_debug dev_size_mb=500
-    sudo pvcreate /dev/sdb
-    sudo vgcreate vg /dev/sdb
-    sudo lvcreate -n ThinPoolLV -L 200M vg
-    sudo lvcreate -n ThinMetaLV -L 50M vg
-    sudo lvconvert \
-        --type thin-pool --poolmetadata vg/ThinMetaLV vg/ThinPoolLV -f -y
-    sudo lvchange --errorwhenfull y vg/ThinPoolLV
-fi
+sudo modprobe scsi_debug dev_size_mb=500
+sudo pvcreate /dev/sdb
+sudo vgcreate vg /dev/sdb
+sudo lvcreate -n ThinPoolLV -L 200M vg
+sudo lvcreate -n ThinMetaLV -L 50M vg
+sudo lvconvert \
+    --type thin-pool --poolmetadata vg/ThinMetaLV vg/ThinPoolLV -f -y
+sudo lvchange --errorwhenfull y vg/ThinPoolLV
 sudo lvcreate -n ThinLV -V 1g --thinpool ThinPoolLV vg
 sudo dd if=/dev/zero of=/dev/mapper/vg-ThinLV bs=1M count=201
+if [ "CHK$1" != "CHK" ];then
+    exit
+fi
 sudo lvremove vg/ThinLV -y
+sudo vgchange -an
+sudo vgremove vg -y
+sudo modprobe -r dm-thin-pool
+sleep 5
+sudo modprobe -r scsi_debug
