2295803a64fcba4396ad1ce84d63fafa1d177023
Gris Ge <fge@redhat.com>
2018-01-17 23:59:15 +0800

    Add script for multipath error injection.
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ dmmp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+sudo modprobe scsi_debug dev_size_mb=100 \
+    vpd_use_hostno=0 add_host=8 max_luns=2
+sudo modprobe dm-multipath
+sudo systemctl restart multipathd
+sudo multipath -l
+echo offline | sudo tee /sys/block/sdp/device/state
+if [ "CHK$1" != "CHK" ];then
+    exit
+fi
+sleep 30
+sudo systemctl stop multipathd
+sudo multipath -F
+sleep 5
+sudo modprobe -r scsi_debug
